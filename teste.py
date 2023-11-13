3f8206bafef6654b8b7b2a9575ca7672483216d0
lanerson <45043973+lanerson@users.noreply.github.com>
2023-11-13 09:40:34 -0300

    .

---
+++ teste.py
@@ -0,0 +1,16 @@
+import csv
+import random
+import numpy as np
+def gerarTabelas(caminho_arquivo):
+    a = np.array([random.uniform(1,2) for i in range(18)]).reshape(3,6)
+    with open(caminho_arquivo, 'a') as arquivo_csv: 
+        for coluna in a:
+            for num in coluna:
+                arquivo_csv.write(str(num))
+                arquivo_csv.write(',')
+            arquivo_csv.write('\n')
+        arquivo_csv.close()
+        
+            
+            
+gerarTabelas('./teste.csv')
